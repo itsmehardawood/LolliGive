687bbab6fd7f54cb82cf34724727fa21e5574ec2
itsmehardawood <mehar.dawood.official@gmail.com>
2025-09-29 11:39:53 +0500

    Firebase Credentials Updated

--- src/app/lib/firebase.js
+++ src/app/lib/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAblQ6mZFfSMR8UExYBKX0ePbSP0-ZoN68",
-  authDomain: "lolligive.firebaseapp.com",
-  projectId: "lolligive",
-  storageBucket: "lolligive.firebasestorage.app",
-  messagingSenderId: "502014264447",
-  appId: "1:502014264447:web:edbfa35b251dcd7757af75",
-  measurementId: "G-64EM26JYS5"
+  apiKey: "AIzaSyDqKNn1eMDFsdD9JW1LYqKntBKMQ3gVeuA",
+  authDomain: "lolligive-llc.firebaseapp.com",
+  projectId: "lolligive-llc",
+  storageBucket: "lolligive-llc.firebasestorage.app",
+  messagingSenderId: "58751947659",
+  appId: "1:58751947659:web:de164c916128e61a57958e",
+  measurementId: "G-12N2S8JYRZ"
 };
 
 // Initialize Firebase
